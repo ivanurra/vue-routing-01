431c5dce85f9eccbdb7a7efceca02ee5ffb43317
Ivan Urra <ivan.urra.wd@gmail.com>
2021-04-03 11:51:51 +0200

    catch all routes

--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true }
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', redirect: '/teams'}
   ],
   linkActiveClass: 'active'
 });
