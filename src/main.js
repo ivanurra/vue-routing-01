2c5585a731aebb9f2218afbc4d0fbf835575325c
Ivan Urra <ivan.urra.wd@gmail.com>
2021-04-07 13:33:53 +0200

    controlling scroll

--- src/main.js
+++ src/main.js
@@ -23,7 +23,14 @@ const router = createRouter({
     { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', redirect: '/teams'}
   ],
-  linkActiveClass: 'active'
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
 });
 
 const app = createApp(App)
